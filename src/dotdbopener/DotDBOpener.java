6afb5ac9b327bd05f3d4a8ca515488bc181e8fc3
ar <ar@winpc.ar-home-inner.invalid>
2017-02-18 02:32:45 +0900

    SQLite用コード追加

--- src/dotdbopener/DotDBOpener.java
+++ src/dotdbopener/DotDBOpener.java
@@ -5,17 +5,38 @@
  */
 package dotdbopener;
 
+import java.sql.*;
+import org.apache.logging.log4j.*;
+
 /**
  *
  * @author ar
  */
 public class DotDBOpener {
 
+	private static final Logger logger = LogManager.getLogger();
+
 	/**
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
 		// TODO code application logic here
+		DotDBOpener ddo = new DotDBOpener();
+
+		logger.info("{}", "DBをオープンします。");
+		ddo.openDB();
+	}
+
+	private void openDB() {
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection("jdbc:sqlite:db/EasyLog.db");
+
+			con.close();
+		} catch (SQLException ex) {
+			logger.fatal("{}", ex);
+		}
+
 	}
 
 }
